964e25e5f546140d8f52e88d11a312d3ba93507e
Marcin Kubala <kubala.webdesign@gmail.com>
2011-04-17 20:37:07 +0200

    +Interfejs IEngine

---
+++ extraCell/domain/IEngine.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace extraCell.domain
+{
+    interface IEngine
+    {
+        Cell getCell(int col, int row);
+        Cell getCell(String str);
+        void setCell(int col, int row, Cell value);
+        void setCell(int col, int row, String formula);
+        void addColumn();
+        void addRow();
+    }
+}
