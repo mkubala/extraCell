cc74c78b836888002a42aee8e757a18e10a339e5
Marcin Kubala <kubala.webdesign@gmail.com>
2011-05-16 23:09:08 +0200

    dodana obsluga importu/eksportu XML

---
+++ extraCell/formula/functions/roznica.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace extraCell.formula.functions
+{
+    class roznica : IFunction
+    {
+        public Object run(Object[] args)
+        {
+            Double res = 0d;
+            foreach (Object arg in args)
+                if (arg.ToString().Length > 0)
+                {
+                    foreach (String s in arg.ToString().Split(','))
+                        res -= Convert.ToDouble(s.Trim().Replace('.', ','));
+                }
+                else
+                {
+                    return "###";
+                }
+
+            return res.ToString();
+        }
+    }
+}
