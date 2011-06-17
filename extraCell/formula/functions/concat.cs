96cb6b49b27f4e778b470aec97c5325874d081e4
Marcin Kubala <kubala.webdesign@gmail.com>
2011-06-17 17:59:46 +0200

    alpha-1

---
+++ extraCell/formula/functions/concat.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace extraCell.formula.functions
+{
+    class concat : IFunction {
+        public Object run(Object[] args)
+        {
+            StringBuilder res = new StringBuilder();
+            System.Diagnostics.Debug.WriteLine("args = " + args.ToString());
+            foreach (Object arg in args)
+            {
+                System.Diagnostics.Debug.WriteLine("arg = " + arg.ToString());
+                res.Append(arg);
+            }
+            return res.ToString();
+        }
+    }
+}
